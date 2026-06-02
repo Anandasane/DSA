e432e044f3406e44d509281f561679443a162fc7
Anandasane <anandasane4@gmail.com>
2026-06-02 17:34:59 +0530

    added new file in rescursion

--- recursion/oneToN.java
+++ recursion/oneToN.java
@@ -6,11 +6,32 @@ public class oneToN {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         n = sc.nextInt();
-        printNumbers(1);
+        print(n);
     }
-    public static void printNumbers(int x){
-        if (x > n) return;
-        System.out.print(x + " ");
-        printNumbers(x  + 1);
+    public static void print(int n){
+        if(n==0) return;
+        System.out.print(n+" ");
+        print(n-1);
+        if(n!=1) System.out.print(n + " ");
+
     }
+    
+    // public static void print(int n){
+    //     if(n==0) return;
+    //     print(n-1);
+    //     System.out.print(n + " ");
+    // }
+
+    // public static void printNumbers(int x){
+    //     if (x > n) return;
+    //     System.out.print(x + " ");
+    //     printNumbers(x  + 1);
+    // }
+    //
+    // public static void print(int x , int n){
+    //     if(x > n )return;
+    //     System.out.print(x + " ");
+    //     print(x + 1, n);
+    // }
+
 }
