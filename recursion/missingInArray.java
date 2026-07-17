4daccfc5becdd9b4a5dea7d290cae20e0c977ba3
Anandasane <anandasane4@gmail.com>
2026-07-17 15:54:26 +0530

    added new file

--- recursion/missingInArray.java
+++ recursion/missingInArray.java
@@ -1,44 +1,49 @@
 import java.util.*;
 
-public class missingInArray{
+public class missingInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the sieze of the array: ");
+        System.out.println("Enter the size of the array: ");
         int n = sc.nextInt();
         int[] ar = new int[n];
         System.out.println("Enter the elements in the array: ");
-        for(int i =0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             ar[i] = sc.nextInt();
         }
 
-        for(int i=0;i<n;i++){
-            System.out.print(ar[i]+" ");
+        System.out.print("Array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(ar[i] + " ");
         }
 
-
-        missingNum(ar);
-
+        int missing = missingNum(ar);
+        System.out.println("\nMissing number: " + missing);
     }
+
     public static int missingNum(int arr[]) {
-        int n = 1 + arr.length;
+        int n = arr.length + 1;
         int i = 0;
-        while(i<arr.length){
-            if(arr[i]==i+1 || arr[i]==n) i++;
-            else{
-                int idx =arr[i] -1;
-                swap(arr,i,idx);
-                
+        while (i < arr.length) {
+            if (arr[i] == i + 1) {
+                i++;
+            } else {
+                int idx = arr[i] - 1;
+                if (idx < arr.length && arr[i] != arr[idx]) {
+                    swap(arr, i, idx);
+                } else {
+                    i++;
+                }
             }
         }
-        for(i=0;i<arr.length;i++){
-            if(arr[i] != i+1) return i+1;
-            
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
         }
         return n;
-        
-        
     }
-    public static void swap(int[] arr, int i , int idx){
+
+    public static void swap(int[] arr, int i, int idx) {
         int temp = arr[i];
         arr[i] = arr[idx];
         arr[idx] = temp;
